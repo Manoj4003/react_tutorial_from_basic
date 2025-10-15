6f49717a2e112a1fc3c8f37ec98b8593c4bee2cf
Manoj4003 <manojknr16@gmail.com>
2025-10-15 21:54:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import CounterApp from './components/counterApp/counterApp'
+
+
+function App() {
+
+
+  return (
+    <>
+
+   <CounterApp/>
+    </>
+  )
+}
+
+export default App
