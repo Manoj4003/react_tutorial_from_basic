d6445b1f6e98ba3f0a23a0909f66807ebd968fa2
Manoj4003 <manojknr16@gmail.com>
2025-10-23 22:03:07 +0530

    updated code

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const CounterApp = () => {
-  let [count,setCount]=useState(0);
-  function handleIncrease(){
-    setCount((previousCount)=>{ return previousCount+1});
-  }
-  function handleDecrease(){
-    setCount((previousCount)=>{ return previousCount-1});
-  }
-  function handleReset(){
-    setCount(count=0)
-  }
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const handleDecrease = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
-    <div>
-      <h1>Count:{count}</h1>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Count: {count}</h1>
       <button onClick={handleIncrease}>Increase</button>
       <button onClick={handleDecrease}>Decrease</button>
       <button onClick={handleReset}>Reset</button>
     </div>
-  )
-}
+  );
+};
 
 export default CounterApp;
